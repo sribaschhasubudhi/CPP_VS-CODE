58fe40b9bae835c278dd57bc5dbd0bbecec0f2ab
Sribaschha Subudhi <sribaschha22nov@gmail.com>
2026-08-30 01:17:15 +0530

    Incomplete code

---
+++ Codes/BASIC EXERCISE/PRIMITIVE DATA.cpp
@@ -0,0 +1,9 @@
+/*
+In this code, we will check whether primitive values crosses the limit or not.
+We will check using the following details of a student:
+    * Application ID (long long)
+    * Gender (char)
+    * Batch (short)
+    * Semester (short)
+    * GPA ()
+*/
