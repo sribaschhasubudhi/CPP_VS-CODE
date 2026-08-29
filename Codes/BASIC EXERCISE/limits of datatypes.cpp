643bf15ae0bbc20aacde9898ca214ca8726c3d71
Sribaschha Subudhi <sribaschha22nov@gmail.com>
2026-08-30 01:27:04 +0530

    checking limits of integer and decimal numbers

--- Codes/BASIC EXERCISE/limits of datatypes.cpp
+++ Codes/BASIC EXERCISE/limits of datatypes.cpp
@@ -18,4 +18,9 @@ int main()
     std::cout << "The minimum limit of char data type: " << CHAR_MIN << "\n";
     std::cout << "\nThe maximum limit of short data type: " << SHRT_MAX << "\n";
     std::cout << "The minimum limit of short data type: " << SHRT_MIN << "\n\n";
+
+    std::cout << "The maximum limit of float datatype: " << FLT_MAX << "\n";
+    std::cout << "The minimum limit of float datatype: " << FLT_MIN << "\n";
+    std::cout << "The maximum limit of double datatype: " << DBL_MAX << "\n";
+    std::cout << "The minimum limit of double datatype: " << DBL_MIN << "\n";
 }
