0a52ef099015cb3eb6fe0f1ec8361bc5f661f60c
Sribaschha Subudhi <sribaschha22nov@gmail.com>
2026-08-31 21:34:57 +0530

    checking the errors

---
+++ DEMO_bounds checking.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int years[] {2019,2026,1992,1971,1947,2007};
+    std::cout << "India got independence in the year " << years[1] << std::endl;
+    std::cout << "My date of birth is " << years[10] << std::endl;
+}
