3784ba6210e8caa3d9e8a9d805704d1712216566
Sribaschha Subudhi <sribaschha22nov@gmail.com>
2026-08-31 23:54:29 +0530

    trying bounds checking

--- Codes/DEMO_bounds checking.cpp
+++ Codes/DEMO_bounds checking.cpp
@@ -4,5 +4,7 @@ int main()
 {
     int years[] {2019,2026,1992,1971,1947,2007};
     std::cout << "India got independence in the year " << years[1] << std::endl;
-    std::cout << "My date of birth is " << years[10] << std::endl;
+//    std::cout << "My date of birth is " << years[10] << std::endl;
+/* remove the above single-line comment and run the code
+   to see the warning for bounds checking */
 }
