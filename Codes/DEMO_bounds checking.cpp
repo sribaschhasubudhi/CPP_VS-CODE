7ea472c9b7757c6868552274fa8281cbe9e4c112
Sribaschha Subudhi <sribaschha22nov@gmail.com>
2026-09-01 01:18:27 +0530

    using bounds checking

--- Codes/DEMO_bounds checking.cpp
+++ Codes/DEMO_bounds checking.cpp
@@ -6,6 +6,6 @@ int main()
     std::cout << "India got independence in the year " << years[4] << std::endl;
     // std::cout << "My date of birth is " << years[10] << std::endl;
     // years[10]=2007;
-    /* remove the above single-line comment and run the code to see the warning for bounds checking */
+    /* remove the above single-line comments and run the code to see the warnings for bounds checking */
     return 0;
 }
